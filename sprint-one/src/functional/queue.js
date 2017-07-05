8623aa6d8ce9b357f49b5fab0d6f304e451aa0da
Hack Reactor Student <student@hackreactor.com>
2017-07-05 15:18:32 -0700

    functionnal stack and queue for functional shared pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var temp = {};
+    var i = 1;
+    for (var key in storage) {
+      temp[i] = storage[key];
+      i++;
+    }
+    temp[0] = value;
+    storage = temp;
   };
 
   someInstance.dequeue = function() {
+    var deletedElement = storage[Object.keys(storage).length-1];
+    delete(storage[Object.keys(storage).length-1]);
+    return deletedElement;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
