5837713d71d119bccc8ea4039e0cf44d3720faec
Hack Reactor Student <student@hackreactor.com>
2017-07-05 18:10:53 -0700

    func shared in progress

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  jQuery.extend(storage, queueMethods);
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    //var temp = {};
+    var i = 1;
+    for (var key in this){
+      this[i] = this[key];
+    }
+    this[0] = value;
+    //this = temp;
+  },
+  dequeue: function() {
+    var temp = this[Object.keys(this).length-4];
+    console.log('test');
+    delete this[Object.keys(this).length-4];
+    
+    return temp;
+  },
+  size: function() {
+    return Object.keys(this).length-3;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  // storage.size = stackMethods.size;
+  // storage.push = stackMethods.push;
+  // storage.pop = stackMethods.pop;
+  jQuery.extend(storage, stackMethods);
+  //storage.stackMethods = Stack.stackMethods
+  return storage; 
 };
 
-var stackMethods = {};
+
+  // var size = function() {
+  //   return Object.keys(this).length;
+  // };
+
+
+  var stackMethods = {
+  size : function(){
+    return Object.keys(this).length-3;
+  },
+  push : function(value){
+   this[Object.keys(this).length] = value; 
+  },
+  pop : function() {
+    var deleted = this[Object.keys(this).length-1];
+    delete this[Object.keys(this).length-1];
+    return deleted;
+  }
+};
 
 
